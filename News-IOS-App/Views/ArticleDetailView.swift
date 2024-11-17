7e95ab66ee8f921b4c36904e08a9f959c6813b59
Jatin Foujdar <jatin321001@gmail.com>
2024-11-17 23:44:13 +0530

    detailed commit

---
+++ News-IOS-App/Views/ArticleDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ArticleDetailView.swift
+//  News-IOS-App
+//
+//  Created by jatin foujdar on 17/11/24.
+//
+
+import SwiftUI
+
+struct ArticleDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArticleDetailView()
+}
