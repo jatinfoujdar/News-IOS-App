28b96217f97ad474186e45b90f9dfabca76f6363
Jatin Foujdar <jatin321001@gmail.com>
2024-11-17 15:03:29 +0530

    Initial Commit

---
+++ News-IOS-App/News_IOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_IOS_AppApp.swift
+//  News-IOS-App
+//
+//  Created by jatin foujdar on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct News_IOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
