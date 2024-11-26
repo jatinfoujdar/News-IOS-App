3364597ce949cf58238b3ae518b2edfc726118e9
Jatin Foujdar <jatin321001@gmail.com>
2024-11-26 17:16:45 +0530

    scroll , image timer commit

--- News-IOS-App/Views/LandingPageView.swift
+++ News-IOS-App/Views/LandingPageView.swift
@@ -2,21 +2,33 @@ import SwiftUI
 
 struct LandingPageView: View {
     @State private var currentIndex = 0
+    @State private var timer: Timer? = nil
     let images = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
     
     var body: some View {
         ZStack {
-         
-            ForEach(images.indices, id: \.self) { index in
-                Image(images[index])
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(10)
-                    .frame(width: 150, height: 150)
-                    .position(x: CGFloat(index) * 160 + 80, y: 300)
+           
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 20) {
+                    ForEach(images.indices, id: \.self) { index in
+                        Image(images[index])
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(10)
+                            .frame(width: 150, height: 150)
+                        
+                    }
+                }
+                .padding(.bottom,300)
+                .padding(.horizontal, 20)
             }
+            .frame(height: 200)
+            .onAppear {
+              
+                startImageTimer()
+            }
+
             
-       
             Image(images[currentIndex])
                 .resizable()
                 .scaledToFit()
@@ -25,10 +37,11 @@ struct LandingPageView: View {
                 .scaleEffect(1.2)
                 .shadow(radius: 10)
                 .onTapGesture {
+                 
                     currentIndex = (currentIndex + 1) % images.count
                 }
             
-        
+       
             VStack {
                 Text("Explore Your News Flasher")
                     .font(.largeTitle)
@@ -39,7 +52,7 @@ struct LandingPageView: View {
             }
             .padding(.horizontal)
 
-          
+           
             VStack {
                 Spacer()
                 HStack {
@@ -49,13 +62,23 @@ struct LandingPageView: View {
                         .padding()
                         .background(Color.green)
                         .cornerRadius(10)
-
                 }
                 .padding(.bottom, 40)
             }
         }
         .edgesIgnoringSafeArea(.all)
         .background(Color.black)
+        .onDisappear {
+          
+            timer?.invalidate()
+        }
+    }
+
+    private func startImageTimer() {
+    
+        timer = Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { _ in
+            currentIndex = (currentIndex + 1) % images.count
+        }
     }
 }
 
