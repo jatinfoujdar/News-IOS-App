057d2854e670c74497b7b3686654c9b02794cbfb
Jatin Foujdar <jatin321001@gmail.com>
2024-11-26 22:51:17 +0530

    header commit

--- News-IOS-App/Views/HeaderView.swift
+++ News-IOS-App/Views/HeaderView.swift
@@ -1,15 +1,30 @@
-//
-//  HeaderView.swift
-//  News-IOS-App
-//
-//  Created by jatin foujdar on 18/11/24.
-//
-
 import SwiftUI
 
 struct HeaderView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            
+            Image("list")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 24, height: 24)
+
+            Spacer()
+
+           
+            Image("search")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 24, height: 24)
+                .padding()
+
+          
+            Image("bell")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 24, height: 24)
+        }
+        .padding()  
     }
 }
 
