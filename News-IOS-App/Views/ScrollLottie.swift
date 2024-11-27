0b060309f621e9faa36c4c2476e8c9b7e60e6539
Jatin Foujdar <jatin321001@gmail.com>
2024-11-27 22:00:27 +0530

    animation name commit

--- News-IOS-App/Views/ScrollLottie.swift
+++ News-IOS-App/Views/ScrollLottie.swift
@@ -2,9 +2,8 @@ import SwiftUI
 import Lottie
 
 struct ScrollLottie: View {
-    let animationNames = ["Assest.json","Science.json",  "Data.json", "Business.json",
-    "Sports.json","Tech.json","Travel.json","Share Market.json"]
-   
+    let animationNames = ["Assest.json","Science.json","Data.json","Business.json",
+                          "Sports.json","Tech.json","Travel.json","Share Market.json"]
     
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
@@ -12,8 +11,12 @@ struct ScrollLottie: View {
                 ForEach(animationNames, id: \.self) { animationName in
                     VStack {
                         LottieCard(animationName: animationName)
+                        Text(animationName)
+                            .font(.caption)
+                            .foregroundColor(.black)
+                            .padding(.top, 8)
                     }
-                    .frame(width: 80, height: 80)
+                    .frame(width: 100, height: 140)
                     .background(Color.gray.opacity(0.1))
                     .cornerRadius(8)
                 }
